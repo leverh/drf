468db0a9d2ff1edc2ba7e3ba606e0ea760791581
leverh <leverh@gmail.com>
2023-08-14 15:44:08 +0000

    Add allowed hosts and CLIENT_ORIGIN_DEV URL

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import re
 from pathlib import Path
 import os 
 import dj_database_url
@@ -70,7 +71,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = ['8000-leverh-drf-3lech7xyrcc.ws-eu103.gitpod.io',
-                 'https://drfapiappy-f779baf3bc38.herokuapp.com/'   
+                 os.environ.get('ALLOWED_HOST'), 
                 ]
 
 
@@ -115,13 +116,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
